505722e116a595215c30621c4b37829ad9c8a6bd
Alissa Troiano <alissatroianonyc@gmail.com>
2020-10-30 19:59:06 +0000

    Add functionality to display session profile upon login

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -54,15 +55,18 @@ def login():
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
+ 
         if existing_user:
-            # ensure hashed password matches user input
+            # check input matches hashed password
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+
             else:
-                # invalid password match
+                # if password is invalid
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
@@ -72,6 +76,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username="username")
 
 
 if __name__ == "__main__":
